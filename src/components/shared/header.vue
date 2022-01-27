490ec7a4769e9b810f2492871882ac3acf3daa8b
Youssef ElAsfoury <youssefelasfoury@gmail.com>
2022-01-27 23:01:29 +0200

    Header and Routes Updates

--- src/components/shared/header.vue
+++ src/components/shared/header.vue
@@ -1,14 +1,37 @@
 <template>
   <v-col cols="12">
-    <div class="header__top_menu">
-      <div class="top_menu__container">
-        <nav>
-          <h1>The header Component</h1>            
-        </nav>
-      </div>
-    </div>
+      <v-app-bar  dense dark elevation="1">
+        <v-app-bar-title>The App Name</v-app-bar-title>
+        <v-spacer></v-spacer>
+        <template v-slot:extension>
+          <v-tabs class="v-tabs-bar" color="#1d1d1d">
+            <v-tab><router-link to="/" active-class="active" tag="a" exact>Home</router-link></v-tab>
+            <v-tab><router-link to="/link1" active-class="active" tag="a">Link 1</router-link></v-tab>
+          </v-tabs>
+        </template>
+      </v-app-bar>      
   </v-col>
 </template>
+<script>
+export default {
+  
+}
+</script>
 <style lang="scss" scoped>
-
+  .v-tabs-bar {
+    background: #eeeeee !important;
+    .v-tab {
+      text-transform: none !important;
+      a{
+        color: #1d1d1d;
+        text-decoration: none;
+      }
+      &::before{
+        background: #252525;
+      }
+    }
+    .v-tab--active{
+      background: #ccc;
+    }
+  }
 </style>
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
-import  Home from './components/Home.vue';
+import Home from './components/Home.vue';
+import ComponentName from './components/Component.vue';
 
 export const routes = [
   { path: '', component: Home },
+  { path: '/link1', component: ComponentName }
 ];
